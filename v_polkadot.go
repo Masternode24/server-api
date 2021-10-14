01d4e1d4478acb7007514170119a358732778854
Terra Validator <node@blckchn.pro>
2021-10-14 22:05:37 +0200

    UPDATE

--- v_polkadot.go
+++ v_polkadot.go
@@ -7,7 +7,7 @@ import (
 )
 
 func VerService() string {
-	command := []string{"--version"}
+	command := []string{"--version | awk '{print $2}' | head -1"}
 	out, err := exec.Command("/root/polkadot/./target/release/polkadot'", command...).Output()
 	if err != nil {
 		fmt.Println("an error has occurred while checking")
