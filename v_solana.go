67ae4534215a3b064c691366c28d844978ab8434
Terra Validator <node@blckchn.pro>
2021-10-15 17:19:02 +0200

    UPDATE

--- v_solana.go
+++ v_solana.go
@@ -6,7 +6,7 @@ import (
 )
 
 func VerService() string {
-	out, err := exec.Command("bash", "-c", "solana version | awk '{print $2}' | tr -d '\n'").Output()
+	out, err := exec.Command("bash", "-c", "solana --version | awk '{print $2}' | tr -d '\n'").Output()
 	if err != nil {
 		log.Fatal(err)
 	}
