27bd5a61190f8f61143713d0f1751131f8a9d235
[ G'Lab ] Team <92644489+g3n3s1sl4b@users.noreply.github.com>
2021-11-10 19:29:42 +0100

    Create up_iris.go

---
+++ up_iris.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+)
+
+func UpService() string {
+	out, err := exec.Command("bash", "-c", "iriscli status | jq . | grep 'catching_up' | awk '{print $2}' | tr -d '\n'").Output()
+	if err != nil {
+		log.Fatal(err)
+	}
+	verString := string(out[:])
+	return verString
+}
